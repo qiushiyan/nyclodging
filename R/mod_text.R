0d83908a24ab271ee2792c8f73674df979785682
qiushi <qiushi.yann@gmail.com>
2021-11-30 23:54:05 -0600

    vignette on modeling

--- R/mod_text.R
+++ R/mod_text.R
@@ -11,32 +11,61 @@
 mod_text_ui <- function(id){
   ns <- NS(id)
   
-  select_ui <- col_6(
-    
+  select_ui <- col_8(
+    col_12(textInput(ns("description"), 
+                     "description of the house", 
+                     placeholder = "2b2b with awesome harbour view", 
+                     width = "100%")), 
+    tags$div(
+        col_6(
+          sliderInput(ns("lon"), "longitude", min = -74.2, max = -73.7, value = -73.9), 
+        ), 
+        col_6(
+          sliderInput(ns("lat"), "latitude", min = 40.5, max = 40.9, value = 40.7)
+        )
+    ) %>% tagAppendAttributes(style = "display:flex;pad"), 
+    col_12(
+      p("click on the map the pick a location"), 
+      leafletOutput(ns("plot"))
+    )
+  )
+  
+  predict_ui <- col_4(
+    verbatimTextOutput(ns("result"))
   )
   
   tagList(
-    h1("text"),
-    leafletOutput(ns("plot"))
+    select_ui, 
+    predict_ui 
   )
 }
     
 #' text Server Functions
 #'
 #' @noRd 
-mod_text_server <- function(id){
-  moduleServer( id, function(input, output, session){
+mod_text_server <- function(id) {
+  moduleServer( id, function(input, output, session) {
     ns <- session$ns
     
-    output$plot <- renderLeaflet(
-      leaflet() %>% 
-        setView(lng = -71.0589, lat = 42.3601, zoom = 12) %>% 
-        addTiles()
+    house_icon <- makeIcon(
+      iconUrl = app_sys("app/www/house_icon.png")
     )
-    
+  
     observeEvent(input$plot_click, {
-      print(input$plot_click)
+      updateSliderInput(session, "lon", value = isolate(input$plot_click$lng))
+      updateSliderInput(session, "lat", value = isolate(input$plot_click$lat))
     })
+    
+    output$plot <- renderLeaflet({
+        leaflet() %>% 
+          addTiles() %>% 
+          addMarkers(lng = input$lon, 
+                     lat = input$lat,
+                     popup = "my awesome house",
+                     icon = house_icon)
+    
+        })
+    
   })
 }
     
